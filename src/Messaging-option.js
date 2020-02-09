426535df248890c741f090098a1d390d79c970e9
max <mchen210@uottawa.ca>
2020-02-09 01:05:33 -0500

    fix messaging-option

--- src/Messaging-option.js
+++ src/Messaging-option.js
@@ -1,40 +1,14 @@
-import options from "./Messaging-option";
-import Paho from "paho-mqtt";
-
-class Messaging extends Paho.Client {
-
-	constructor() {
-		super(options.invocationContext.host, Number(options.invocationContext.port), options.invocationContext.clientId);
-		this.onMessageArrived = this.handleMessage.bind(this);
-		this.callbacks = [];
-	}
-
-	connectWithPromise() {
-		return new Promise((resolve, reject) => {
-			options.onSuccess = resolve;
-			options.onFailure = reject;
-			this.connect(options);
-		});
-	}
-
-	// called when the client loses its connection
-	onConnectionLost(responseObject) {
-		if (responseObject.errorCode !== 0) {
-			console.log("Connection lost with Solace Cloud");
-		}
-		// Add auto connect logic with backoff here if you want to automatically reconnect
-	}
-
-	register(callback) {
-		this.callbacks.push(callback);
-	}
-	
-	// called when a message arrives
-	handleMessage(message) {
-		console.log("Received message", message.payloadString);
-		this.callbacks.forEach(callback => callback(message));
-	}
-}
-
-const messaging = new Messaging();
-export default messaging;
+export default {
+	userName: "solace-cloud-client",
+	password: "<connection details password>",
+	invocationContext: {
+		host: "<WebSocket Secured MQTT Host>",
+		port: 8443,
+		clientId: ""
+	},
+	timeout: 3,
+	keepAliveInterval: 60,
+	cleanSession: true,
+	useSSL: true,
+	reconnect: true
+};
