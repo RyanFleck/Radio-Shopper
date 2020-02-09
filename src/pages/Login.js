10363f409a07e11f5c4ecd15c99a32dfa339a686
ohayat1 <omar.hayat613@gmail.com>
2020-02-09 07:56:41 -0500

    Added Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import PubSub from '../components/PubSub';
+import { Tab } from '@material-ui/core';
+import Logo from './logo.png';
 
 function Login(props) {
     return (
         <React.Fragment>
 
         <div><h1>Login</h1></div>
+        <form>
+        <label>
+            User name {'   '}
+            <input type="text" name="name" />
+        </label>
+        </form>
+        <form>
+        <label>
+            Password {'   '}
+            <input type="text" name="name" />
+        </label>
+        </form>
+        <Button onClick={PubSub.actionOne} color="primary" >
+            Login
+        </Button>
+        <img src= {Logo} />
+        <Button onClick={PubSub.actionOne} color="primary" >
+            Sign Up
+        </Button>
 
         </React.Fragment>
+        
     );
 }
 
